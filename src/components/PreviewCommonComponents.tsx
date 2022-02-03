ba55221ce5d65f3a54668b74aa8cfb02a6e003a3
Vladimir Shits <rav9aa@gmail.com>
2022-02-04 02:31:36 +0300

    changed Preview page

--- src/components/PreviewCommonComponents.tsx
+++ src/components/PreviewCommonComponents.tsx
@@ -3,15 +3,24 @@ import SuperButton from "../common/c2-SuperButton/SuperButton";
 import SuperCheckbox from "../common/c3-SuperCheckbox/SuperCheckbox";
 import SuperEditableSpan from "../common/c4-SuperEditableSpan/SuperEditableSpan";
 import SuperRadio from "../common/c6-SuperRadio/SuperRadio";
+import SuperSelect from "../common/c5-SuperSelect/SuperSelect";
+import {useState} from "react";
 
 export const PreviewCommonComponents =()=>{
+    const arr = ['x', 'y', 'z']
+    const [value, onChangeOption] = useState(arr[1])
     return(
         <div>
-            <SuperInputText/>
+            <SuperInputText error={'error'}/>
             <SuperButton>Button</SuperButton>
             <SuperCheckbox> Checkbox</SuperCheckbox>
             <SuperEditableSpan/>
             <SuperRadio>Radio</SuperRadio>
+            <SuperSelect
+                options={arr}
+                value={value}
+                onChangeOption={onChangeOption}
+                />
         </div>
     )
 }
