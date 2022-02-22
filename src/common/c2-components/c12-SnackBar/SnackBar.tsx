4ef44dcf54321c6bd52d9e1d44c2d0dae9fde351
Vladimir Shits <rav9aa@gmail.com>
2022-02-22 16:15:19 +0300

    changed SnackBar component

--- src/common/c2-components/c12-SnackBar/SnackBar.tsx
+++ src/common/c2-components/c12-SnackBar/SnackBar.tsx
@@ -3,45 +3,28 @@ import {Alert, Snackbar} from "@mui/material";
 import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "../../../components/bll/b2-store/store";
 import {deletePopupMessageAC, PopupMessageType} from "../../../components/bll/b1-reducers/app/app-reducer";
-//import {setErrorAC} from "../../../components/bll/b1-reducers/app/app-reducer";
 
-type CustomLinkPropsType = {
-    address: string
-    text: string
-}
 
-type  StatusType = 'success' | 'failed'
-export const handleActionStatus = (status: StatusType, error: string = '') => {
-
-}
-
-export const SnackBar = (props: any) => {
-    // const error = useSelector<AppStateType, string>((state) => state.app.error)
-    // const success = useSelector<AppStateType, string>((state => state.app.success))
+export const SnackBar = () => {
 
     const popupMessages = useSelector<AppStateType, PopupMessageType[]>((state => state.app.popupMessages))
     const dispatch = useDispatch()
 
-    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    const handleClose = (event?: React.SyntheticEvent | Event | React.MouseEvent<HTMLDivElement, MouseEvent>, reason?: string) => {
         if (reason === 'clickaway') {
             return;
         }
         debugger
-        //@ts-ignore
-        const newPopupArr = popupMessages.filter((i) => i.message !== event.currentTarget.id)
-        dispatch(deletePopupMessageAC(newPopupArr))
-    };
+       if(event !==null && event !== undefined ){
+           //@ts-ignore
+           const newPopupArr = popupMessages.filter((i) => i.message !== event.currentTarget.id)
+           dispatch(deletePopupMessageAC(newPopupArr))
+       }
+    }
 
-    /*    const res = popupMessages.map(i =><div id ={i.message} onClick={handleClose}>
-            <Alert style={{marginTop: '15px'}} variant={'standard'} onClose={handleClose} severity={i.type}
-                   sx={{width: '100%'}}>
-                {i.message}
-            </Alert>
-        </div>
-        )*/
-    const res = popupMessages.map(i => <div id={i.message} onClick={handleClose}
+    const res = popupMessages.map(i => <div key={i.message} id={i.message} onClick={(e)=>handleClose(e)}
                                             style={{width: '300px', wordBreak: 'break-all'}}>
-            <Snackbar open={popupMessages[0] !== undefined} autoHideDuration={3000} onClose={handleClose}
+            <Snackbar open={popupMessages[0] !== undefined} autoHideDuration={3000} onClose={(e)=>handleClose(e)}
                       style={{
                           position: "relative",
                           bottom: "24px",
@@ -71,12 +54,3 @@ export const SnackBar = (props: any) => {
         </div>
     )
 }
-
-
-//  <Snackbar open={popupMessages[0] !== undefined} /*autoHideDuration={6000} onClose={handleClose}*/ key={2} id={'12'}>
-//    <Stack sx={{width: '100%'}} spacing={2} id={'12'}
-//       style={{display: "flex", flexDirection: 'column'}}
-//  >
-//{res}
-//  </Stack>
-//  </Snackbar>
