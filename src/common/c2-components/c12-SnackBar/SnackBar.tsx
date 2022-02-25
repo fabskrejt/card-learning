fdcbe7a074b3783c5933ca5cba3ecff254da4966
Vladimir Shits <rav9aa@gmail.com>
2022-02-25 15:43:47 +0300

    bugFix SnackBar

--- src/common/c2-components/c12-SnackBar/SnackBar.tsx
+++ src/common/c2-components/c12-SnackBar/SnackBar.tsx
@@ -10,7 +10,7 @@ export const SnackBar = () => {
     let popupMessages = useSelector<AppStateType, PopupMessageType[]>((state => state.app.popupMessages))
     const dispatch = useDispatch()
 
-    const handleClose = (event?: SyntheticEvent<Element, Event>| Event, reason?: string, id?: string) => {
+    const handleClose = (event?: SyntheticEvent<Element, Event> | Event, reason?: string, id?: string) => {
         if (reason === 'clickaway') {
             return;
         }
@@ -21,32 +21,18 @@ export const SnackBar = () => {
         }
 
     }
-    // @ts-ignore
-    /*    const message = popupMessages.map(i => <div  key={i.id+i.type} id={i.id} onClick={handleClose}
-                                                    style={{width: '300px', wordBreak: 'break-all', transition:'10'}}>
-                <div
-                          style={{
-                              position: "relative",
-                              bottom: "24px",
-                              left: "24px",
-                              right: "auto"
-                          }}>
-                    <Snackbar title={'adasd'} open={popupMessages[0] !== undefined} autoHideDuration={3000} id={i.id} style={{position:'revert'}}  onClose={()=>handleClose(i.id)}>
-                    <Alert style={{marginTop: '15px'}} variant={'standard'} onClose={handleClose} severity={i.type}
-                           sx={{width: '100%'}}>
-                        {i.message}
-                    </Alert>
-                    </Snackbar>
-                </div>
-            </div>
-        )*/
 
+    const message = popupMessages.map(i =>
+        <Snackbar
+            key={i.id}
+            onClose={(ev, res) =>
+                handleClose(ev, res, i.id)}
+            open={popupMessages[0] !== undefined}
+            autoHideDuration={3000}
+            style={{position: 'relative'}}>
 
-    const message = popupMessages.map(i => <Snackbar onClose={(ev, res) => handleClose(ev, res,i.id)}
-                                                     open={popupMessages[0] !== undefined} autoHideDuration={3000}
-                                                     id={i.id} style={{position: 'relative'}}>
-
-            <Alert style={{marginTop: '15px',wordBreak: 'break-all',width: '300px'}} variant={'standard'} onClose={(e) => handleClose(e,'',i.id)} severity={i.type}
+            <Alert style={{marginTop: '15px', wordBreak: 'break-all', width: '300px'}} variant={'standard'}
+                   onClose={(e) => handleClose(e, '', i.id)} severity={i.type}
                    sx={{width: '100%'}}>
                 {i.message}
             </Alert>
